81346a4f2764407bb7cd9b775f5c19dd9fb6d45d
busaikuna <kunaabus@gmail.com>
2024-11-29 20:23:41 -0300

    add task model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+STATUS = [("pendente", "pendente"), ("em andamento", "em andamento"), ("concluido", "concluido")]
+
+class Task():
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=255)
+    status = models.CharField(choices=STATUS, default="pendente")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
